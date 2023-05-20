1ca8e27f216effaf9f6c8188403bc0ab399169e0
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-21 00:30:45 +0200

    Products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ProductSeeder;
 use Database\Seeders\CategorySeeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(CategorySeeder::);
-        $this->call(ProductSeeder::);
+        $this->call(CategorySeeder::class);
+        $this->call(ProductSeeder::class);
     }
 }
