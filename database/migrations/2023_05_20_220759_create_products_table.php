726994215712a110938a66f4fc34f12362d3ba35
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-21 00:15:20 +0200

    Products

--- database/migrations/2023_05_20_220759_create_products_table.php
+++ database/migrations/2023_05_20_220759_create_products_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->constrained();
-            $table->string();
+            $table->string('name');
+            $table->string('name');
             $table->timestamps();
         });
     }
