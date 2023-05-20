e630672997a9cedff70ff008c0e39c210a729c37
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-21 01:11:31 +0200

    Products

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', [ProductController::class, 'index']);
 
