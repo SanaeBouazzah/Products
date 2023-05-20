7d571f63813f20d119c888514dac43f354cb02c8
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-21 00:15:41 +0200

    Products

--- database/migrations/2023_05_20_220759_create_products_table.php
+++ database/migrations/2023_05_20_220759_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('category_id')->constrained();
             $table->string('name');
-            $table->string('name');
+            $table->integer('price');
             $table->timestamps();
         });
     }
