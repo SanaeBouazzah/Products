81bde1733e8321d54ce611ddaba8ee877d96ed0f
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-05-21 00:40:41 +0200

    Products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
@@ -11,6 +12,6 @@ class Product extends Model
     protected $fillable = ['name', 'category_id', 'price'];
     public function category()
     {
-      
+      return $this->belongsTo(Category::class);
     }
 }
